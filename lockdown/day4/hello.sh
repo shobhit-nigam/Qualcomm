44e2b30e29e2da746df7d6b14e7a65b9ad2d9af2
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2020-05-21 17:53:18 +0530

    Add files via upload

---
+++ lockdown/day4/hello.sh
@@ -0,0 +1,7 @@
+pwd
+echo "hello"
+sleep 4
+clear
+qualcomm
+sleep 4
+ls
---
+++ lockdown/day4/one.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void main()
+	{
+	int varx = 33;
+	printf("hello world\n");
+	printf("varx = %d\n, varx);
+	}
